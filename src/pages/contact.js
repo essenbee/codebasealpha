4dce3412cf9d82b4ac1c1a9a00f1e9fe9cc8ec76
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2020-03-09 22:44:31 +0000

    Modified Netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -26,6 +26,7 @@ const ContactPage = () => (
                       method="post" 
                       data-netlify="true"
                       data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="contact" />
                     <input type="hidden" name="bot-field" />
                     <div className="field">
                         <label for="name">Name
